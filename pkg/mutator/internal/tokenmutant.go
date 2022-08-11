46777739217e2c86321f3d72f8a318c12720ddaf
Davide Petilli <davide@petilli.me>
2022-08-11 09:37:48 +0200

    mutator: refactor apply and rollback

--- pkg/mutator/internal/tokenmutant.go
+++ pkg/mutator/internal/tokenmutant.go
@@ -17,18 +17,19 @@
 package internal
 
 import (
+	"bytes"
 	"go/ast"
 	"go/printer"
 	"go/token"
 	"os"
+	"path/filepath"
 
-	"github.com/go-gremlins/gremlins/pkg/log"
 	"github.com/go-gremlins/gremlins/pkg/mutant"
 )
 
 // TokenMutant is a mutant.Mutant of a token.Token.
 type TokenMutant struct {
-	pkg         string
+	origFile    []byte
 	fs          *token.FileSet
 	file        *ast.File
 	tokenNode   *NodeToken
@@ -39,9 +40,8 @@ type TokenMutant struct {
 }
 
 // NewTokenMutant initialises a TokenMutant.
-func NewTokenMutant(pkg string, set *token.FileSet, file *ast.File, node *NodeToken) *TokenMutant {
+func NewTokenMutant(set *token.FileSet, file *ast.File, node *NodeToken) *TokenMutant {
 	return &TokenMutant{
-		pkg:       pkg,
 		fs:        set,
 		file:      file,
 		tokenNode: node,
@@ -78,48 +78,67 @@ func (m *TokenMutant) Pos() token.Pos {
 	return m.tokenNode.TokPos
 }
 
-// Pkg returns the package name to which the mutant belongs.
-func (m *TokenMutant) Pkg() string {
-	return m.pkg
-}
-
-// Apply saves the original token.Token of the mutant.Mutant and sets the
-// current token from the tokenMutations table.
+// Apply overwrites the source code file with the mutated one. It also
+// stores the original file in the TokenMutant in order to allow
+// Rollback to put it back later.
 //
 // To apply the modification, it first removes the source code file which
 // contains the mutant position, then it writes it back with the mutation
 // applied.
+// The removal of the file is necessary because it might be a hard link
+// to the original file, and, if it was modified in place, it would modify
+// the original. Removing the link and re-writing the file preserves the
+// original to be modified.
+//
+// Apply also puts back the original Token after the mutated file write.
+// This is done in order to facilitate the atomicity of the operation,
+// avoiding locking in a method and unlocking in another.
 func (m *TokenMutant) Apply() error {
+	filename := filepath.Join(m.workDir, m.Position().Filename)
+	var err error
+	m.origFile, err = os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
 	m.actualToken = m.tokenNode.Tok()
 	m.tokenNode.SetTok(tokenMutations[m.Type()][m.tokenNode.Tok()])
 
-	return m.writeOnFile()
-}
+	w := &bytes.Buffer{}
+	err = printer.Fprint(w, m.fs, m.file)
+	if err != nil {
+		return err
+	}
 
-// Rollback puts back the original token of the TokenMutant, then it writes
-// it on the actual file which contains the position of the mutant.Mutant.
-func (m *TokenMutant) Rollback() error {
+	// We need to remove the file before writing because it can be
+	// a hard link to the original file.
+	err = os.RemoveAll(filename)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, w.Bytes(), 0600)
+	if err != nil {
+		return err
+	}
+
+	// Rollback here to facilitate the atomicity of the operation.
 	m.tokenNode.SetTok(m.actualToken)
 
-	return m.writeOnFile()
+	return nil
 }
 
-func (m *TokenMutant) writeOnFile() error {
-	err := removeFile(m.fs, m.tokenNode.TokPos, m.workDir)
-	if err != nil {
-		return err
-	}
-	f, err := openFile(m.fs, m.tokenNode.TokPos, m.workDir)
-	if err != nil {
-		return err
-	}
-	defer closeFile(f)
-	err = printer.Fprint(f, m.fs, m.file)
-	if err != nil {
-		return err
-	}
+// Rollback puts back the original file after the test and cleans up the
+// TokenMutant to free memory.
+//
+// It isn't necessary to remove the file before writing as it is done in
+// Apply, because in this case, we can be sure the file is not a hard link,
+// since Apply already made it a concrete one.
+func (m *TokenMutant) Rollback() error {
+	defer m.resetOrigFile()
+	filename := filepath.Join(m.workDir, m.Position().Filename)
 
-	return nil
+	return os.WriteFile(filename, m.origFile, 0600)
 }
 
 // SetWorkdir sets the base path on which to Apply and Rollback operations.
@@ -131,35 +150,7 @@ func (m *TokenMutant) SetWorkdir(path string) {
 	m.workDir = path
 }
 
-func removeFile(fs *token.FileSet, tokPos token.Pos, workdir string) error {
-	file := fs.File(tokPos)
-	file.Name()
-	if workdir != "" {
-		workdir += "/"
-	}
-	err := os.RemoveAll(workdir + file.Name())
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func openFile(fs *token.FileSet, tokPos token.Pos, workdir string) (*os.File, error) {
-	file := fs.File(tokPos)
-	if workdir != "" {
-		workdir += "/"
-	}
-	f, err := os.OpenFile(workdir+file.Name(), os.O_CREATE|os.O_WRONLY, 0600) //nolint:nosnakecase
-	if err != nil {
-		return nil, err
-	}
-
-	return f, err
-}
-
-func closeFile(f *os.File) {
-	if err := f.Close(); err != nil {
-		log.Errorln("an error occurred while closing the mutated file")
-	}
+func (m *TokenMutant) resetOrigFile() {
+	var zeroByte []byte
+	m.origFile = zeroByte
 }
